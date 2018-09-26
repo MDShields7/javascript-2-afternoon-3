607aef1a1fb3159b27dd366f215c2a5490718497
Matt Shields <matthew.duncan.shields@gmail.com>
2018-09-25 22:00:26 -0700

    finish required work

--- practice.js
+++ practice.js
@@ -28,6 +28,9 @@
 */
 
 // Code Here 
+function first (arr, cb){
+  cb(arr[0]);
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,7 +51,9 @@ first(names, function(firstName){
 */
 
 //Code Here
-
+function last (arr, cb){
+  cb(arr[arr.length-1]);
+}
 // Do not edit the code below.
 last(names, function(lastName){
   console.log('The last name in names is ' + lastName);
@@ -66,7 +71,9 @@ last(names, function(lastName){
 */
 
 //Code Here
-
+function multiply(num1, num2, cb){
+  cb(num1*num2)
+}
 // Do not edit the code below.
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
@@ -85,7 +92,15 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
-
+function contains (arr, name, cb){
+  for(let i = 0; i< arr.length; i++){
+    if (arr[i] === name){
+      cb(true);
+    } else {
+      cb(false);
+    }
+  }
+}
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
   if(result === true){
@@ -106,7 +121,16 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
-
+function uniq(arr, cb){
+  for (let i = 0; i<arr.length; i++){
+    for(let j = arr.length; j>0; j--){
+      if (i !== j && arr[i] === arr[j]){
+        arr.splice(j,1);
+      }
+    }
+  }
+  cb(arr);
+}
 // Do not edit the code below.
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
@@ -123,6 +147,14 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
+// function each (arr, cb){
+//   arr.map(cb(arr[item],[item]))
+// }
+function each(arr, cb){
+  for (let i = 0; i < arr.length; i++){
+    cb(arr[i], i);
+  }
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -140,7 +172,13 @@ each(names, function(item, indice){
 */
 
 // Code here
-
+function getUserById (arr, id, cb) {
+    for (let i = 0; i<arr.length;i++) {
+      if (arr[i]['id'] === id) {
+        cb(arr[i]);
+      }
+    }
+}
 // Do not edit the code below.
 var users = [
   {
